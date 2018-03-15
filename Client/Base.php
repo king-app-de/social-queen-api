b25ebf195f3ca4748ab3f887c3afe0bbdfaf3583
grzesiek <g.pierzakowski@gmail.com>
2018-03-15 11:27:06 +0100

    error handling

--- Client/Base.php
+++ Client/Base.php
@@ -21,10 +21,23 @@ abstract class Base
         $this->client = $client;
     }
 
-    public function __call(string $name, ?array $parameters = [])
+    public function __call($name, $parameters = [])
     {
-        $parameters[1]['headers'] = $this->wrap($parameters[1]['headers'] ?? ['clientKey', 'deviceSecret', 'deviceKey', 'apiVersion']);
-        return $this->client->$name(...$parameters);
+        $parameters[1]['headers'] = $this->wrap(isset($parameters[1]['headers']) ? $parameters[1]['headers'] : ['clientKey', 'deviceSecret', 'deviceKey', 'apiVersion']);
+        try {
+            return $this->client->$name($parameters[0], $parameters[1]);
+        } catch (\Throwable $exception) {
+            if ($exception->getCode() === 403) {
+                $body = $exception->getResponse()->getBody()->getContents();
+                throw new \Exception(json_decode($body));
+            }
+            if ($exception->getCode() === 401) throw new \Exception("Not authorized");
+            if ($exception->getCode() === 406) {
+                $body = $exception->getResponse()->getBody()->getContents();
+                throw new \Exception("Data is not acceptable. Check the api version you are using. Error: " . $body);
+            }
+            throw new \Exception("Communication error");
+        }
     }
 
     private function wrap(array $headers): array
